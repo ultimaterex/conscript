1d5ed0833585a56784a0b6bd0d5e71f59f325ba0
Selby Baidjnath <selby@serubii.com>
2024-10-01 20:30:15 -0300

    Refactor main.go to add /info endpoint and return application information as JSON

--- main.go
+++ main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
+	"os"
+	"runtime"
+	"time"
 )
 
 type contextKey string
+
 const appVersion string = "0.1.1"
 const keyServerAddress contextKey = "server_address"
+
+var startTime = time.Now()
 
 const (
 	pathRoot               = "/"
 	pathHealth             = "/health"
+	pathInfo               = "/info"
 	pathContainers         = "/containers"
 	getContainerPath       = "/container/"
 	getContainerHealthPath = getContainerPath + "health/"
@@ -35,10 +43,47 @@ func getHealth(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "OK\n")
 }
 
+type Info struct {
+	ApplicationVersion string `json:"application_version"`
+	Hostname           string `json:"hostname"`
+	Uptime             string `json:"uptime"`
+	CurrentTime        string `json:"current_time"`
+	GoVersion          string `json:"go_version"`
+}
+
+func getInfo(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("got /info request from %s\n", ctx.Value(keyServerAddress))
+	hostname, err := os.Hostname()
+	if err != nil {
+		http.Error(w, "Failed to get hostname", http.StatusInternalServerError)
+		return
+	}
+
+	uptime := time.Since(startTime).String()
+	currentTime := time.Now().Format(time.RFC1123)
+	goVersion := runtime.Version()
+
+	info := Info{
+		ApplicationVersion: appVersion,
+		Hostname:           hostname,
+		Uptime:             uptime,
+		CurrentTime:        currentTime,
+		GoVersion:          goVersion,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(info); err != nil {
+		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc(pathRoot, getRoot)
 	mux.HandleFunc(pathHealth, getHealth)
+	mux.HandleFunc(pathInfo, getInfo)
 
 	mux.HandleFunc(pathContainers, listContainers)
 	mux.HandleFunc(getContainerPath, getContainer(getContainerPath))
